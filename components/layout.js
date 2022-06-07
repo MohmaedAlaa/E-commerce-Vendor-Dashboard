6b210f6fd06dafef9cf49a8f64f135fed62ecd61
Mohamed Mostafa <mohamedhadia611@gmail.com>
2022-06-07 20:26:49 +0200

    add table 2

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <>
       <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
-        <aside className="z-30 bg-blue-100 flex-shrink-0 hidden shadow-sm w-64 overflow-y-auto bg-Primary dark:bg-gray-800 lg:block">
+        <aside className="z-30 bg-[#3D897A] flex-shrink-0 hidden shadow-sm w-64 overflow-y-auto text-white font-bold lg:block">
           <div className="flex flex-col gap-4 p-4">
             <Link href="/">
               <a>Home</a>
@@ -14,6 +14,9 @@ export default function Layout({ children }) {
             </Link>
             <Link href="/Table">
               <a>Table</a>
+            </Link>
+            <Link href="/Table2">
+              <a>Table2</a>
             </Link>
           </div>
         </aside>
