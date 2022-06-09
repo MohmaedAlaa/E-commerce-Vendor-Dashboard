d21c22b4381de68c9ec48f2137d6f1d2454a07d7
Mohamed Mostafa <mohamedhadia611@gmail.com>
2022-06-09 09:50:36 +0200

    update status in products table

--- pages/products.js
+++ pages/products.js
@@ -129,7 +129,7 @@ export default function Products() {
               SubCategory: "Clothing",
               Quantity: "120",
               UnitPrice: "€ 30",
-              Status: true,
+              Status: "Active",
               date: "24/5/2022",
               image:
                 "https://images.unsplash.com/photo-1625910513520-bed0389ce32f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=60",
@@ -141,7 +141,7 @@ export default function Products() {
               SubCategory: "Clothing",
               Quantity: "120",
               UnitPrice: "€ 30",
-              Status: false,
+              Status: "Active",
               date: "24/5/2022",
               image:
                 "https://images.unsplash.com/photo-1625910513520-bed0389ce32f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=60",
@@ -153,7 +153,7 @@ export default function Products() {
               SubCategory: "Clothing",
               Quantity: "120",
               UnitPrice: "€ 30",
-              Status: true,
+              Status: "false",
               date: "24/5/2022",
               image:
                 "https://images.unsplash.com/photo-1625910513520-bed0389ce32f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=60",
